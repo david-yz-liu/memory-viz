06a85f4f80117801145cd8991d5cc6196f9326a2
Mimis Chlympatsos <129094642+mimischly7@users.noreply.github.com>
2023-05-26 14:36:20 +0300

    Update code documentation (#1)
    Co-authored-by: umututku03 <egemen.umut@mail.utoronto.ca>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-// const fs = require("fs")
+// A library allowing interaction with the file system (e.g. creating new files)
 import fs from "fs"
+// The library allowing for the production of sketches that appear to be hand-drawn
 import rough from "roughjs/bundled/rough.esm.js"
 
 const { DOMImplementation, XMLSerializer } = require("@xmldom/xmldom")
 
+/** The class representing the memory model diagram of the given block of code. */
 class MemoryModel {
-    constructor(options) {
+    /**
+     * Create the memory model diagram.
+     * @property {object} svg - An svg 'Element' object from the DOM (Document Object Model) module.
+     *                          Scalable Vector Graphics (svg) is an image format based on geometry.
+     * @property {object} rough_svg - Instantiating a RoughSVG object by passing the root svg node (this.svg) to the
+     *                                'rough.svg()' method. As per the documentation of the 'rough' library,
+     *                                "RoughSVG provides the main interface to work with this library".
+     *
+     * NOTE: Other properties of this class are a consequence of the constant 'config' object in the bottom of this file.
+     *       These include 'id_colour', 'obj_min_width', and 'font_size'. The 'config' constant also contains default
+     *       values for these properties.
+     *       Moreover, the user can optionally set custom width and height for the canvas by passing them as attributes
+     *       to the 'options' argument. To see this in practice, see ./examples/demo.js.
+     *
+     */
+    constructor(options)    {
         options = options || {}
         if (options.browser) {
             this.document = document
@@ -21,10 +38,19 @@ class MemoryModel {
             "http://www.w3.org/2000/svg",
             "svg"
         )
+
+        // Defining 'this.width', 'this.height', and 'this.rough_svg'.
         this.svg.setAttribute("width", options.width || 800)
         this.svg.setAttribute("height", options.height || 800)
         this.rough_svg = rough.svg(this.svg)
 
+        // 'config' is a constant object holding configuration information, defined in the bottom of this file.
+        // In particular, 'config' contains all properties that a 'MemoryModel' object must have, ---as well as their
+        // corresponding default values---. However, with the 'options' parameter, the user has the ability to override
+        // these default values by passing in a custom object with the desired attribute values.
+        // For instance, if the user runs
+        //                      m = new MemoryModel({font_size: 17}),
+        // then the default 'font_size' value of 20 will be overridden and 'm.font_size' will evaluate to 17.
         for (const key in config) {
             this[key] = options.hasOwnProperty(key) ? options[key] : config[key]
         }
@@ -33,6 +59,8 @@ class MemoryModel {
     /**
      * Save the current image to an SVG file at the given path.
      * If path is undefined, write the svg to stdout instead.
+     * @param path: The repository (local location that the image
+     * will be saved).
      */
     save(path) {
         const xmlSerializer = new XMLSerializer()
@@ -49,51 +77,59 @@ class MemoryModel {
     }
 
     /**
-     * Render the image SVG to a given canvas object.
+     * Render the image (show the output) SVG to a given canvas object.
+     * @param canvas: the element that will be used to draw graphics
      */
     render(canvas) {
         const ctx = canvas.getContext("2d")
-        var image = new Image()
-        var data = "data:image/svg+xml;base64," + window.btoa(this.svg)
+        let image = new Image()
+        let data = "data:image/svg+xml;base64," + window.btoa(this.svg)
         image.src = data
         ctx.drawImage(image, 0, 0)
     }
 
     /**
      * Distribute the object drawing depending on type
-     * @param x: optional value for x coordinate of top left corner
-     * @param y: optional value for y coordinate of top left corner
-     * @param type
-     * @param id: memory address number
+     * @param x: value for x coordinate of top left corner
+     * @param y: value for y coordinate of top left corner
+     * @param type: the data type (e.g. list, int) of the object we want draw
+     * @param id: the hypothetical memory address number
      * @param value: can be passed as a list if type is a collection type
      * @param show_indexes: whether to show list indices
      */
     drawObject(x, y, type, id, value, show_indexes) {
-        if (collections.includes(type)) {
+        if (collections.includes(type)) {  // If the given object is a collection
             if (type === "dict") {
                 this.drawDict(x, y, id, value)
             } else if (type === "set") {
                 this.drawSet(x, y, id, value)
             } else if (type === "list" || type === "tuple") {
-                this.drawList(x, y, type, id, value, show_indexes)
+                this.drawSequence(x, y, type, id, value, show_indexes)
             }
-        } else {
+        } else {  // If the given object is a primitive data type
             this.drawPrimitive(x, y, type, id, value)
         }
     }
 
     /**
      * Draw a primitive object.
+     * @param {number} x: value for x coordinate of top left corner
+     * @param {number} y: value for y coordinate of top left corner
+     * @param {string} type: the primitive data type (e.g. boolean, int) of the object we want draw
+     * @param {number} id: the hypothetical memory address number
+     * @param {*} value: the value of the primitive object
      */
     drawPrimitive(x, y, type, id, value) {
-        // Draw object box
+        // Adjust and draw object box (see 'config' object for the information on the attributes)
         let box_width = Math.max(
             this.obj_min_width,
             this.getTextLength(String(value)) + this.obj_x_padding
         )
         this.drawRect(x, y, box_width, this.obj_min_height)
 
-        // Draw double box for immutable types
+        // For immutable types we need a double box, so we add another box that will contain the one we created.
+        // Coordinate-wise, we utilize 'this.double_rec_sep' (see 'config' for more information).
+        // It represents the space to leave between the inner box and the outer box.
         if (immutable.includes(type)) {
             this.drawRect(
                 x - this.double_rect_sep,
@@ -103,6 +139,7 @@ class MemoryModel {
             )
         }
 
+        // Initializing the text that will be displayed, using the 'type' and 'value' arguments.
         let display_text
         if (type === "bool") {
             display_text = value ? "True" : "False"
@@ -110,7 +147,8 @@ class MemoryModel {
             display_text = JSON.stringify(value)
         }
 
-        // Draw the value
+        // Actually drawing the text to be displayed on our canvas by utilizing the helper 'drawText' instance method.
+        // Note that if the value is null or undefined, nothing will be drawn
         if (value !== null && value !== undefined) {
             this.drawText(
                 display_text,
@@ -125,27 +163,36 @@ class MemoryModel {
     }
 
     /**
-     * Draw id and type properties
+     * Draw the id and type properties of an object with a given type and id.
+     * @param {number} id: the hypothetical memory address number
+     * @param {string} type: the data type of the given object
+     * @param {number} x: value for x coordinate of top left corner
+     * @param {number} y: value for y coordinate of top left corner
+     * @param {number} width: The width of the given box (rectangle)
      */
     drawProperties(id, type, x, y, width) {
+
+        // Adjust the id box by taking into account 'this.min_width'.
         let id_box = Math.max(
             this.prop_min_width,
             this.getTextLength(`id${id}`) + 10
         )
 
-        // adjust length of type name box
+        // Adjust the id box by taking into account 'this.prop_min_width'.
         let type_box = Math.max(
             this.prop_min_width,
             this.getTextLength(type) + 10
         )
 
-        // Draw text
+        // Draw the text inside the id box (insert the id of the given object to the id box)
         this.drawText(
             id === null ? "" : `id${id}`,
             x + id_box / 2,
             y + this.font_size * 1.5,
             this.id_color
         )
+
+        // Draw the text inside the type box (insert the data type of the given object to the id box)
         this.drawText(
             type,
             x + width - type_box / 2,
@@ -153,24 +200,44 @@ class MemoryModel {
             this.value_color
         )
 
-        // Draw boxes
+        // Draw boxes (specify the boxes for id and type)
         this.drawRect(x, y, id_box, this.prop_min_height)
         this.drawRect(x + width - type_box, y, type_box, this.prop_min_height)
     }
 
     /**
-     * Draw a list object
-     * @param show_idx: whether to show the indexes of each list element
+     * Draw a sequence object (must be either a list or a tuple).
+     * @param {number} x: value for x coordinate of top left corner
+     * @param {number} y: value for y coordinate of top left corner
+     * @param {string} type: the data type of the given object (tuple or list)
+     * @param {number} id: the hypothetical memory address number
+     * @param {number[]} values: the list of id's corresponding to the values stored in this set.
+     *      NOTE:
+     *          1. This argument MUST be an array, since the built-in 'forEach' method works only for
+     *             (finite) ordered collections (i.e. with indexing). Sets are a type of unordered collection.
+     *          2. The 'values' argument must store the id's and not the actual value of the list elements.
+     *             If the instructor wishes to showcase the corresponding values, it is their responsibility to create
+     *             memory boxes for all elements (with id's that match the id's held in 'values').
+     *
+     * @param {boolean} show_idx: whether to show the indexes of each list element
+     *
+     * Moreover, note that this program does not force that for every id in the values argument there is
+     * a corresponding object (and its memory box) in our canvas.
      */
-    drawList(x, y, type, id, values, show_idx) {
+    drawSequence(x, y, type, id, values, show_idx) {
+      
         // Object width
         let box_width = this.obj_x_padding * 2
-        values.forEach((v) => {
+
+        // For each element of 'values', we increase 'box_width' as required, to make space for all values.
+        values.forEach((v) => {  // v represents one single value
             box_width += Math.max(
                 this.item_min_width,
                 this.getTextLength(v === null ? "" : `id${v}`) + 10
             )
         })
+
+        // Final 'box_width' adjustment; ensuring the box is at least as wide as required by 'this.obj_min_width'.
         box_width = Math.max(this.obj_min_width, box_width)
 
         // Box height
@@ -181,6 +248,8 @@ class MemoryModel {
 
         // Draw box
         this.drawRect(x, y, box_width, box_height)
+
+        // As with all primitives, we are drawing a second enclosing box to highlight that this object is immutable
         if (immutable.includes(type)) {
             this.drawRect(
                 x - this.double_rect_sep,
@@ -190,7 +259,7 @@ class MemoryModel {
             )
         }
 
-        // Draw element boxes
+        // Draw the boxes for each element
         let curr_x = x + this.item_min_width / 2
         let item_y =
             y +
@@ -198,7 +267,7 @@ class MemoryModel {
             (this.obj_min_height -
                 this.prop_min_height -
                 this.item_min_height) /
-                2 // y coordinate of list items
+            2  // y coordinate of list items
         if (show_idx) {
             item_y += this.list_index_sep
         }
@@ -232,12 +301,26 @@ class MemoryModel {
     }
 
     /**
-     * Draw a set object
+     * Draw a set object.
+     * @param {number} x: value for x coordinate of top left corner
+     * @param {number} y: value for y coordinate of top left corner
+     * @param {number} id: the hypothetical memory address number
+     * @param {number[]} values: the list of id's corresponding to the values stored in this set.
+     *      NOTE:
+     *          1. This argument MUST be an array, since the built-in 'forEach' method works only for
+     *             (finite) ordered collections (i.e. with indexing). Sets are a type of unordered collection.
+     *          2. The 'values' argument must store the id's and not the actual value of the list elements.
+     *             If the instructor wishes to showcase the corresponding values, it is their responsibility to create
+     *             memory boxes for all elements (with id's that match the id's held in 'values').
+     *
+     * Moreover, note that this program does not force that for every id in the values argument there is
+     * a corresponding object (and its memory box) in our canvas.
      */
     drawSet(x, y, id, values) {
-        // Object width
+
+        // Adjust the object width (the width of the box)
         let box_width = this.obj_x_padding * 2
-        values.forEach((v) => {
+        values.forEach((v) => { // v represents each value in this collection
             box_width += Math.max(
                 this.item_min_width,
                 this.getTextLength(v === null ? "" : `id${v}`) + 10
@@ -246,10 +329,10 @@ class MemoryModel {
         box_width = Math.max(this.obj_min_width, box_width)
         box_width += ((values.length - 1) * this.item_min_width) / 4 // Space for separators
 
-        // Draw box
+        // Draw box which represents the set object
         this.drawRect(x, y, box_width, this.obj_min_height)
 
-        // Draw element boxes
+        // Draw element boxes for each value in this collection
         let curr_x = x + this.item_min_width / 2
         let item_y =
             y +
@@ -257,7 +340,7 @@ class MemoryModel {
             (this.obj_min_height -
                 this.prop_min_height -
                 this.item_min_height) /
-                2 // y coordinate of list items
+            2 // y coordinate of list items
         let item_text_y = item_y + this.item_min_height / 2 + this.font_size / 4
 
         values.forEach((v, i) => {
@@ -304,6 +387,10 @@ class MemoryModel {
 
     /**
      * Draw a dictionary object
+     * @param {number} x: value for x coordinate of top left corner
+     * @param {number} y: value for y coordinate of top left corner
+     * @param {number} id: the hypothetical memory address number
+     * @param {object} obj: the object that will be drawn
      */
     drawDict(x, y, id, obj) {
         let box_width = this.obj_min_width
@@ -323,12 +410,16 @@ class MemoryModel {
                 this.item_min_width,
                 this.getTextLength(idv + 5)
             )
+
+            // Draw the rectangles representing the keys
             this.drawRect(
                 x + this.obj_x_padding,
                 curr_y,
                 key_box,
                 this.item_min_height
             )
+
+            // Draw the text inside the keys
             this.drawText(
                 idk,
                 x + this.item_min_width + 2,
@@ -342,9 +433,9 @@ class MemoryModel {
             box_width = Math.max(
                 box_width,
                 this.obj_x_padding * 2 +
-                    key_box +
-                    value_box +
-                    2 * this.font_size
+                key_box +
+                value_box +
+                2 * this.font_size
             )
             box_height += 1.5 * this.item_min_height
         }
@@ -367,12 +458,15 @@ class MemoryModel {
                 this.value_color
             )
 
+            // Draw the rectangle for values
             this.drawRect(
                 x + box_width / 2 + this.font_size,
                 curr_y,
                 value_box,
                 this.item_min_height
             )
+
+            // Draw the text for the values
             this.drawText(
                 idv,
                 x + box_width / 2 + this.font_size + value_box / 2,
@@ -392,7 +486,12 @@ class MemoryModel {
 
     /**
      * Draw a custom class.
-     * @param stack_frame: set to true if you are drawing a stack frame
+     * @param  {number} x: value for x coordinate of top left corner
+     * @param {number} y: value for y coordinate of top left corner
+     * @param {string} name: the name of the class
+     * @param {number} id: the hypothetical memory address number
+     * @param {object} attributes: the attributes of the given class
+     * @param {boolean} stack_frame: set to true if you are drawing a stack frame
      */
     drawClass(x, y, name, id, attributes, stack_frame) {
         // Get object's width
@@ -404,7 +503,7 @@ class MemoryModel {
         if (longest > 0) {
             box_width = longest + this.item_min_width * 3
         }
-        // Adjust for class name
+        // Adjust for the class name
         box_width = Math.max(
             box_width,
             this.prop_min_width + this.getTextLength(name) + 10
@@ -415,7 +514,7 @@ class MemoryModel {
         if (Object.keys(attributes).length > 0) {
             box_height =
                 ((this.item_min_width * 3) / 2) *
-                    Object.keys(attributes).length +
+                Object.keys(attributes).length +
                 this.item_min_width / 2 +
                 this.prop_min_height
         } else {
@@ -471,7 +570,13 @@ class MemoryModel {
     }
 
     /**
-     * Draw a rectangle
+     * Draw a rectangle that will be used to represent the objects.
+     * @param {number} x: value for x coordinate of top left corner
+     * @param {number} y: value for y coordinate of top left corner
+     * @param {number} width: the width of the rectangle
+     * @param {number} height: the height of the rectangle
+     * @param {object | undefined} style: if specified an object with style properties for a Rough.js object, as per the
+     *                        Rough.js API. For instance, {fill: 'blue', stroke: 'red'}.
      */
     drawRect(x, y, width, height, style) {
         if (style === undefined) {
@@ -484,7 +589,15 @@ class MemoryModel {
 
     /**
      * Draw given text
+     * @param {string} text: The text message that will be displayed
+     * @param {number} x: value for x coordinate of top left corner
+     * @param {number} y: value for y coordinate of top left corner
+     * @param {string} colour: The colour of the text that will be displayed. Must be in the form "rgb(..., ..., ...)".
+     * @param {string} align: The text anchor; one of "start", "middle" or "end".
+     *                        (As per the SVG documentation from developer.mozilla.org)
+     *                        The default value if nothing is supplied (null or undefined) is "middle"
      */
+
     drawText(text, x, y, colour, align) {
         colour = colour || this.text_color
         align = align || "middle"
@@ -503,15 +616,18 @@ class MemoryModel {
     }
 
     /**
-     * Return the length of this text
+     * Return the length of this text.
+     * @param {string} s: The given text.
      */
     getTextLength(s) {
         return s.length * 12
     }
 }
 
+
+// Default configurations we are using
 const config = {
-    rect_style: { stroke: "rgb(0, 0, 0)" },
+    rect_style: {stroke: "rgb(0, 0, 0)"},
     text_color: "rgb(0, 0, 0)", // Default text color
     value_color: "rgb(27, 14, 139)", // Text color for primitive values
     id_color: "rgb(150, 100, 28)", // Text color for object ids
