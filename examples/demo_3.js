72b2f16fff6f62faf772323362345ead13eca744
mimischly7 <mimis.chlympatsos@mail.utoronto.ca>
2023-06-26 23:47:00 +0300

    individual-feedback
    adding sorting options, margin and padding options, differentiating between max_width and actual_width

--- examples/demo_3.js
+++ examples/demo_3.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 const WIDTH = 1300;
 
-const {objs, canvas_height} = drawAutomatedOtherItems(
+const {objs, canvas_height, canvas_width} = drawAutomatedOtherItems(
     [
         {"isClass": true, "name": "Person", "id": 99, "value": {"age": 12, "name": 17}, "stack_frame": false},
         {"isClass": false, "name": "list", "id": 82, "value": [19, 43, 28, 49]},
@@ -18,12 +18,18 @@ const {objs, canvas_height} = drawAutomatedOtherItems(
         {"isClass": false, "name": "dict", "id": 10, "value": {"x": 81, "y": 100, "z": 121}},
         {"isClass": false, "name": "None", "id": 13, "value": "None"}
     ],
-    WIDTH
+    WIDTH,
+    sort_by="height",
+    config_aut={padding: 15, right_margin: 20, bottom_margin: 20}
 )
 
-// console.log(objs)
+console.log("8273490832714-93723489729472372387289203879-2374-32874987230-482-48")
+console.log(objs)
+console.log(canvas_height)
 
-const m = new MemoryModel({width: WIDTH, height: canvas_height});
+// const m = new MemoryModel({width: WIDTH, height: canvas_height});
+console.log(canvas_width)
+const m = new MemoryModel({width: canvas_width, height: canvas_height});
 
 console.log(objs, canvas_height)
 m.drawAll(objs)
