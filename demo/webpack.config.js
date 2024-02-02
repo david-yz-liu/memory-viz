62754c8a7843eb9f62a25c93cbf909131c834e9d
Ziyuan (Jerry) Zhang <ziyuanzhang2019@gmail.com>
2024-02-02 15:15:44 -0500

    set up webpack and package.json

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -8,6 +8,13 @@ module.exports = [
             path: path.resolve(__dirname, "dist"),
             filename: "index.js",
         },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, "dist"),
+            },
+            compress: true,
+            port: 9000,
+        },
         module: {
             rules: [
                 {
