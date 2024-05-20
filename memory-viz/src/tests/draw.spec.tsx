8a2b37ddeb9d43ad30b10780148d6c894fe9a461
Sarah Wang <55766946+sarahsonder@users.noreply.github.com>
2024-05-20 16:44:32 -0400

    Draw function test cases (data types and manual layout) (#30)

--- memory-viz/src/tests/draw.spec.tsx
+++ memory-viz/src/tests/draw.spec.tsx
@@ -27,4 +27,279 @@ describe("draw function", () => {
         const svg: String = m.serializeSVG();
         expect(svg).toMatchSnapshot();
     });
+
+    it("renders a bool", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "bool", id: 32, value: true },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders an int", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "int", id: 32, value: 7 },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a float", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "float", id: 32, value: 7.0 },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a str", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "str", id: 32, value: "winter" },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a set", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "set", id: 32, value: [10, 11, 12] },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders an empty set", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "set", id: 32, value: [] },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a list with indexes showing", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: false,
+                name: "list",
+                id: 32,
+                value: [10, 11, 12],
+                show_indexes: true,
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a list without indexes showing", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "list", id: 32, value: [10, 11, 12] },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders an empty list", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "list", id: 32, value: [] },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a tuple with indexes showing", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: false,
+                name: "tuple",
+                id: 32,
+                value: [10, 11, 12],
+                show_indexes: true,
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a tuple without indexes showing", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "tuple", id: 32, value: [10, 11, 12] },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders an empty tuple", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "tuple", id: 32, value: [] },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a dict", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: false,
+                name: "dict",
+                id: 10,
+                value: { x: 81, y: 100, z: 121 },
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders an empty dict", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "dict", id: 32, value: {} },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders an object with no type and no value", () => {
+        const objects: Array<Object> = [
+            { isClass: false, name: "None", id: 13, value: "None" },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a blank space", () => {
+        const objects: Array<Object> = [
+            { name: "BLANK", width: 100, height: 200 },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a stack frame and an int", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    my_int: 13,
+                },
+                stack_frame: true,
+            },
+            {
+                isClass: false,
+                name: "int",
+                id: 13,
+                value: 7,
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a stack frame using manual layout", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                x: 200,
+                y: 200,
+                name: "__main__",
+                id: null,
+                value: {
+                    lst1: 82,
+                    lst2: 84,
+                },
+                stack_frame: true,
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, false, {
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders a bool using manual layout", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: false,
+                x: 750,
+                y: 250,
+                name: "bool",
+                id: 32,
+                value: true,
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, false, {
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
 });
