c79e1c1c72bb7413c7b05cf2842c7e692a029350
yoonie-jang <yoonie.jang@mail.utoronto.ca>
2024-05-20 19:22:54 -0400

    Add tests for automatic layout and style options

--- memory-viz/src/tests/draw.spec.tsx
+++ memory-viz/src/tests/draw.spec.tsx
@@ -302,4 +302,416 @@ describe("draw function", () => {
         const svg: String = m.serializeSVG();
         expect(svg).toMatchSnapshot();
     });
+
+    it("renders blank spaces in automatic layout", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: false,
+                name: "int",
+                id: 98,
+                value: 42,
+            },
+            {
+                name: "BLANK",
+                width: 100,
+                height: 200,
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 99,
+                value: "life",
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("formats non-stack frame objects in automatic layout", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: false,
+                name: "int",
+                id: 98,
+                value: 42,
+            },
+            {
+                isClass: true,
+                name: "fruit",
+                id: 23,
+                value: {
+                    name: 12,
+                },
+            },
+            {
+                isClass: false,
+                name: "list",
+                id: 54,
+                value: [19, 42, 22, 63],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 12,
+                value: "banana",
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("formats a mix of stack frame/non-stack frame objects in automatic layout", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    a: 7,
+                },
+                stack_frame: true,
+            },
+            {
+                isClass: true,
+                name: "func",
+                id: null,
+                value: {
+                    x: 1,
+                    y: 17,
+                },
+                stack_frame: true,
+            },
+            {
+                isClass: false,
+                name: "list",
+                id: 84,
+                value: [17, 8],
+                show_indexes: true,
+            },
+            {
+                isClass: false,
+                name: "None",
+                id: 10,
+                value: "None",
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders custom style (without presets)", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: false,
+                name: "str",
+                id: 19,
+                value: "David is cool!",
+                style: {
+                    text_id: { fill: "yellow" },
+                    text_type: { fill: "cyan", "font-size": "large" },
+                    text_value: { fill: "black", "font-style": "bold" },
+                    box_id: { fill: "red" },
+                    box_type: { bowing: 8, stroke: "red", strokeWidth: 3 },
+                    box_container: { fill: "green", fillStyle: "zigzag" },
+                },
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders 'highlight' style preset", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    item: 45,
+                },
+                stack_frame: true,
+                style: ["highlight"],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 42,
+                value: "highlight!",
+                style: ["highlight"],
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders 'highlight_id' style preset", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    item: 45,
+                },
+                stack_frame: true,
+                style: ["highlight_id"],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 42,
+                value: "highlight id!",
+                style: ["highlight_id"],
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders 'highlight_type' style preset", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    item: 45,
+                },
+                stack_frame: true,
+                style: ["highlight_type"],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 42,
+                value: "highlight type!",
+                style: ["highlight_type"],
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders 'hide' style preset", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    item: 45,
+                },
+                stack_frame: true,
+                style: ["hide"],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 42,
+                value: "hide!",
+                style: ["hide"],
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders 'hide_id' style preset", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    item: 45,
+                },
+                stack_frame: true,
+                style: ["hide_id"],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 42,
+                value: "hide id!",
+                style: ["hide_id"],
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders 'hide_container' style preset", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    item: 45,
+                },
+                stack_frame: true,
+                style: ["hide_container"],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 42,
+                value: "hide container!",
+                style: ["hide_container"],
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders 'fade' style preset", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    item: 45,
+                },
+                stack_frame: true,
+                style: ["fade"],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 42,
+                value: "fade!",
+                style: ["fade"],
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders 'fade_type' style preset", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    item: 45,
+                },
+                stack_frame: true,
+                style: ["fade_type"],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 42,
+                value: "fade_type!",
+                style: ["fade_type"],
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+
+    it("renders 'fade_id' style preset", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    item: 45,
+                },
+                stack_frame: true,
+                style: ["fade_id"],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 42,
+                value: "fade id!",
+                style: ["fade_id"],
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
+    it("renders combinations of style presets", () => {
+        const objects: Array<Object> = [
+            {
+                isClass: true,
+                name: "__main__",
+                id: null,
+                value: {
+                    item: 45,
+                },
+                stack_frame: true,
+                style: ["highlight", "fade", "hide_id"],
+            },
+            {
+                isClass: false,
+                name: "str",
+                id: 42,
+                value: "combination!",
+                style: ["hide_id", "highlight_type"],
+            },
+        ];
+        const m: InstanceType<typeof MemoryModel> = draw(objects, true, {
+            width: 1300,
+            seed: 12345,
+        });
+        const svg: String = m.serializeSVG();
+        expect(svg).toMatchSnapshot();
+    });
 });
