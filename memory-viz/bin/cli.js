f907a4b42b589a920cdbb187be0260ec0e012a33
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2024-06-09 23:31:23 -0400

    computer is no longer yelling at me

---
+++ memory-viz/bin/cli.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+console.log("Hola amigos");
+
+// args[1] is the path to the JSON file (display error for when the path is not valid)
+// how to run the file?
+// convert JSON to JS (JSON.parse)
+// run memoryviz on the output object (error checking for invalid JSON)
+// save the generated svg
