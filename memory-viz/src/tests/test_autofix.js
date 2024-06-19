48b934dee42bb8ad430fdd3c6664dadb7d2bdf7e
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2024-06-19 16:25:48 -0400

    update yml files

--- memory-viz/src/tests/test_autofix.js
+++ memory-viz/src/tests/test_autofix.js
@@ -1,1 +1,1 @@
-console.log('If autofix ran correctly, this string should be wrapped in double quotes and there should be a comma and the end of this line. This string should also have proper indentation.')
+console.log('If autofix ran correctly, this string should be wrapped in double quotes and there should be a semicolon at the end of this line. This string should also have proper indentation.')
