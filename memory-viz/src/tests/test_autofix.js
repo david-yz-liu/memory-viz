9859d23c164e04da88e1e1267b744da777f2c95f
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2024-06-19 15:51:17 -0400

    test autofix.ci

---
+++ memory-viz/src/tests/test_autofix.js
@@ -0,0 +1,1 @@
+console.log('If autofix ran correctly, this string should be wrapped in double quotes and there should be a comma and the end of this line. This string should also have proper indentation.')
