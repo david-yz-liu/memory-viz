1f6b418e3e200bd3e5afd6c67f7dca2cb57271f1
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2024-08-01 16:42:47 -0400

    setting up website

---
+++ demo/src/AppTwo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export default function AppTwo() {
+    return (
+        <>
+            <Typography>Hello MemoryViz!</Typography>
+        </>
+    );
+}
--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -4,9 +4,26 @@ import App from "./App";
 import "@picocss/pico";
 import "./css/styles";
 
+import AppTwo from "./AppTwo";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#2a6b2c",
+            dark: "#005ea5",
+            light: "#72ac56",
+        },
+    },
+    typography: {
+        fontFamily: "Open Sans, Arial, sans-serif",
+    },
+});
+
 const root = createRoot(document.getElementById("app"));
 root.render(
     <StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <AppTwo />
+        </ThemeProvider>
     </StrictMode>
 );
