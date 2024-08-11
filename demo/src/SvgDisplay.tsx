065ab84a744983751e312dc2ec3d1492a5a054cb
yoonie-jang <yoonie.jang@mail.utoronto.ca>
2024-08-11 02:14:22 -0400

    Adjust SvgDisplay for horizontal layout

--- demo/src/SvgDisplay.tsx
+++ demo/src/SvgDisplay.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import mem from "memory-viz";
 import { configDataPropTypes } from "./MemoryModelsUserInput";
+import { Box, Button } from "@mui/material";
 
 type SvgDisplayPropTypes = {
     jsonResult: object | null;
@@ -12,6 +13,7 @@ export default function SvgDisplay(props: SvgDisplayPropTypes) {
     const canvasRef = useRef(null);
     const canvasWidth = 1300;
     const canvasHeight = 1000;
+    const scale = 0.8;
 
     useEffect(() => {
         if (props.jsonResult !== null) {
@@ -29,11 +31,37 @@ export default function SvgDisplay(props: SvgDisplayPropTypes) {
     }, [props.jsonResult]);
 
     return (
-        <canvas
-            data-testid="memory-models-canvas"
-            ref={canvasRef}
-            width={canvasWidth}
-            height={canvasHeight}
-        />
+        <>
+            <Box sx={{ display: "flex", flexDirection: "row" }}>
+                <Box
+                    overflow="auto"
+                    height={400}
+                    width={700}
+                    sx={[
+                        { border: 1, borderRadius: 2 },
+                        {
+                            "&:hover": {
+                                border: 2,
+                                borderRadius: 2,
+                                borderColor: "primary.main",
+                            },
+                        },
+                    ]}
+                >
+                    <canvas
+                        data-testid="memory-models-canvas"
+                        ref={canvasRef}
+                        width={canvasWidth}
+                        height={canvasHeight}
+                        style={{
+                            width: canvasWidth * scale,
+                            height: canvasHeight * scale,
+                        }}
+                    />
+                </Box>
+            </Box>
+        </>
     );
 }
+
+export type { SvgDisplayPropTypes };
