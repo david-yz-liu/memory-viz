4cd38baa3bf334e0bfcdd6637f0b7368abef99e3
MaxChen102 <max050102@gmail.com>
2025-05-18 23:16:52 -0400

    ctx refactor

--- memory-viz/src/memory_model.ts
+++ memory-viz/src/memory_model.ts
@@ -151,7 +151,9 @@ export class MemoryModel {
      */
     clear(canvas: HTMLCanvasElement): void {
         const ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (ctx !== null) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
     }
 
     /**
