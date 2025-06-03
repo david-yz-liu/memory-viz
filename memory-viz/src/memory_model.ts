fa8e72c3d75f2ee62278441027b7dc34df801901
josh shergill <70562312+jvenchy@users.noreply.github.com>
2025-06-03 18:58:00 -0400

    Enable Typescript NoImplicitAny option (#146)
    Refactors code to resolve all errors reported through the NoImplicitAny option.

--- memory-viz/src/memory_model.ts
+++ memory-viz/src/memory_model.ts
@@ -95,9 +95,9 @@ export class MemoryModel {
 
         // The user must not directly use this constructor; their only interaction should be with 'user_functions.draw'.
         for (const key in config) {
-            this[key] = options.hasOwnProperty(key)
-                ? options[key]
-                : config[key];
+            (this as { [key: string]: any })[key] = options.hasOwnProperty(key)
+                ? options[key as keyof VisualizationConfig]
+                : config[key as keyof typeof config];
         }
     }
 
@@ -582,7 +582,7 @@ export class MemoryModel {
         x: number,
         y: number,
         id: number,
-        obj: object,
+        obj: { [key: string]: any },
         style: Style
     ): Rect {
         let box_width = this.obj_min_width;
@@ -693,7 +693,7 @@ export class MemoryModel {
         y: number,
         name: string,
         id: number,
-        attributes: object,
+        attributes: { [key: string]: any },
         stack_frame: boolean,
         style: Style
     ): Rect {
@@ -843,7 +843,7 @@ export class MemoryModel {
         if (style !== undefined) {
             let new_style = "";
             for (const style_attribute of Object.keys(style)) {
-                new_style += `${style_attribute}:${style[style_attribute]}; `;
+                new_style += `${style_attribute}:${style[style_attribute as keyof CSS.Properties]}; `;
             }
             newElement.setAttribute("style", new_style);
         }
