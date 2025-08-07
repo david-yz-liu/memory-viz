92b96a798258831d425c909b120a71b6885cb023
jvenchy <joshshergill@gmail.com>
2025-08-07 15:31:55 -0400

    cli interactive flag aligns correctly with commander

--- memory-viz/bin/cli.js
+++ memory-viz/bin/cli.js
@@ -112,7 +112,7 @@ function runMemoryViz(jsonContent) {
             roughjs_config: { options: options.roughjsConfig },
             global_style: options.globalStyle,
             theme: options.theme,
-            interactive: !options.noInteractive,
+            interactive: options.interactive,
         });
     } catch (err) {
         console.error(`Error: ${err.message}`);
