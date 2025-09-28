02e98b8a7f5cd724bc57127a25a7a643ce704cab
josh shergill <70562312+jvenchy@users.noreply.github.com>
2025-09-28 10:38:46 -0400

    Refactored CLI to Typescript (#193)

--- memory-viz/webpack.dev.js
+++ memory-viz/webpack.dev.js
@@ -1,10 +1,19 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-    devServer: {
-        static: "./dist",
-    },
-});
+module.exports = [
+    merge(common.libConfig, {
+        mode: "development",
+        devtool: "inline-source-map",
+        devServer: {
+            static: "./dist",
+        },
+    }),
+    merge(common.cliConfig, {
+        mode: "development",
+        devtool: "inline-source-map",
+        devServer: {
+            static: "./dist",
+        },
+    }),
+];
