f852fd5418dc7bd7a35a5b20b3fdc7b7584868bd
Herena <160994961+herenali@users.noreply.github.com>
2025-11-29 02:24:58 +0800

    Extended dict value to accept a list of two-element lists (#232)

--- memory-viz/src/tests/draw.spec.tsx
+++ memory-viz/src/tests/draw.spec.tsx
@@ -587,6 +587,35 @@ describe("draw function", () => {
         expect(svg).toMatchSnapshot();
     });
 
+    it("renders dict with list value", () => {
+        const objects: DrawnEntity[] = [
+            {
+                type: "dict",
+                id: 3,
+                value: [
+                    ["", null],
+                    ["", 1],
+                    ["x", 2],
+                    [" ", 3],
+                    [null, 4],
+                    [undefined, 5],
+                    [0, 6],
+                ],
+            },
+        ];
+        const m: InstanceType<typeof exports.MemoryModel> = draw(
+            objects,
+            true,
+            {
+                width: 1300,
+                roughjs_config: { options: { seed: 12345 } },
+            }
+        );
+        const svg: string = m.serializeSVG();
+        m.save("dist_list4.svg");
+        expect(svg).toMatchSnapshot();
+    });
+
     it("renders dict with custom styling", () => {
         const objects: DrawnEntity[] = [
             {
