bfea99bceb979cb148e1e5538d3ad3d9650fd7bc
David Liu <david@cs.toronto.edu>
2025-12-02 21:45:16 -0500

    Split webpack output into separate files for CJS and ESM

--- memory-viz/webpack.common.js
+++ memory-viz/webpack.common.js
@@ -5,20 +5,8 @@ import webpack from "webpack";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const libConfig = {
-    // target: "web",
+const shared = {
     entry: path.resolve(__dirname, "src/index.ts"),
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "memory-viz.bundle.js",
-        library: {
-            name: "memoryViz",
-            type: "umd",
-            export: "default",
-        },
-        globalObject: "this",
-        clean: false, // Prevent deletion of type declaration files under dist/
-    },
     module: {
         rules: [
             {
@@ -47,7 +35,86 @@ const libConfig = {
     },
 };
 
-const cliConfig = {
+// Browser UMD build
+export const browserConfig = {
+    ...shared,
+    name: "browser",
+    target: "web",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "memory-viz.umd.js",
+        library: {
+            name: "memoryViz",
+            type: "umd",
+            export: "default",
+        },
+        globalObject: "this",
+        clean: false, // Prevent deletion of type declaration files under dist/
+    },
+    resolve: {
+        ...shared.resolve,
+        fallback: {
+            fs: false, // Ensures fs will not be bundled or polyfilled
+        },
+    },
+};
+
+// ESM browser build
+export const browserEsmConfig = {
+    ...shared,
+    target: "web",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "memory-viz.browser.js",
+        libraryTarget: "module",
+        clean: false,
+    },
+    experiments: {
+        outputModule: true, // Required for ESM output
+    },
+    resolve: {
+        ...shared.resolve,
+        fallback: {
+            fs: false, // Ensures fs will not be bundled or polyfilled
+        },
+    },
+};
+
+// CJS Node build
+export const cjsConfig = {
+    ...shared,
+    name: "cjs",
+    target: "node",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "memory-viz.node.cjs",
+        library: {
+            type: "commonjs2", // CommonJS bundle for Node
+            export: "default",
+        },
+        clean: false,
+    },
+};
+
+// ESM Node build
+export const esmConfig = {
+    ...shared,
+    name: "esm",
+    target: "node",
+    experiments: {
+        outputModule: true, // Required for ESM output
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "memory-viz.node.js",
+        library: {
+            type: "module", // ESM bundle
+        },
+        clean: false,
+    },
+};
+
+export const cliConfig = {
     target: "node",
     entry: "./src/cli.ts",
     output: {
@@ -58,10 +125,10 @@ const cliConfig = {
     experiments: {
         outputModule: true,
     },
-    module: libConfig.module,
+    module: shared.module,
     externalsPresets: { node: true },
     externals: [nodeExternals()],
-    resolve: libConfig.resolve,
+    resolve: shared.resolve,
     plugins: [
         new webpack.BannerPlugin({
             banner: "#!/usr/bin/env node",
@@ -70,5 +137,3 @@ const cliConfig = {
         }),
     ],
 };
-
-export { libConfig, cliConfig };
