0ade2da45dfe851cf2f696aaf0c65965ed103eeb
herenali <herenalihoiching@gmail.com>
2026-01-15 11:59:34 -0500

    Update snapshots

--- memory-viz/src/tests/draw.spec.tsx
+++ memory-viz/src/tests/draw.spec.tsx
@@ -669,7 +669,6 @@ describe("draw function", () => {
             }
         );
         const svg: string = m.serializeSVG();
-        m.save("dist_list4.svg");
         expect(svg).toMatchSnapshot();
     });
 
