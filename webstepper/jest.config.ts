3cf284ba4dd82c199053da0023c98f558d63126a
david-yz-liu <david@cs.toronto.edu>
2026-05-13 11:38:22 -0400

    Ignore memory-viz/dist/ in coverage reports

--- webstepper/jest.config.ts
+++ webstepper/jest.config.ts
@@ -29,9 +29,7 @@ const config: Config = {
     coverageDirectory: "coverage",
 
     // An array of regexp pattern strings used to skip coverage collection
-    // coveragePathIgnorePatterns: [
-    //   "/node_modules/"
-    // ],
+    coveragePathIgnorePatterns: ["/node_modules/", "/memory-viz/dist/"],
 
     // Indicates which provider should be used to instrument code for coverage
     // coverageProvider: "babel",
